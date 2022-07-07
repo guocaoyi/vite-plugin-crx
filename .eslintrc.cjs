c2a52020db513a2f74111d75dbda7565f529e878
Yalda <guocaoyi@gmail.com>
2022-07-07 20:08:38 +0800

    feat(init):add preset

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+// @ts-check
+const { defineConfig } = require('eslint-define-config')
+
+module.exports = defineConfig({
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['import'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2021,
+  },
+  rules: {
+    'no-debugger': ['error'],
+    'no-empty': ['warn', { allowEmptyCatch: true }],
+    'no-process-exit': 'off',
+    'no-useless-escape': 'off',
+    'prefer-const': [
+      'warn',
+      {
+        destructuring: 'all',
+      },
+    ],
+    'node/no-missing-import': [
+      'error',
+      {
+        allowModules: ['types', 'estree', 'less', 'sass', 'stylus'],
+        tryExtensions: ['.ts', '.js', '.jsx', '.tsx', '.d.ts'],
+      },
+    ],
+    'node/no-deprecated-api': 'off',
+    'node/no-unpublished-import': 'off',
+    'node/no-unpublished-require': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+  },
+  reportUnusedDisableDirectives: true,
+})
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Vite Plugin for CRX
+
+## Features
+
+- 🚀 Hot Module Replacement via prefresh
+- 🔨 Devtools bridge during development
